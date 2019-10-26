311f8783574fa4942293064672f480ea34589484
sowiks <sowiks@student.mini.pw.edu.pl>
2019-10-26 14:46:39 +0200

    Change interface add tests and travis config

--- R/GetQueue.R
+++ R/GetQueue.R
@@ -1,8 +1,21 @@
+office_ids <- list(
+  USC_Andersa = "5d2e698a-9c31-456b-8452-7ce33e7deb94",
+  UD_Białołęka = "95fee469-79db-4b4b-9ddc-91d49d1f0f51",
+  UD_Bielany = "9c3d5770-57d8-4365-994c-69c5ac4186ee",
+  UD_Ochota = "624d7e2a-bf45-48d6-ba79-8b512e662d1c"
+)
+
+#' @title function GetAvailableOfficeAcronyms
+#' @export
+GetAvailableOfficeAcronyms <- function(){
+  names(office_ids)
+}
+
 #' @title function GetQueue
 #' 
-#' @param nothing, later maybe ID or district name?
+#' @param office_acronym
 #' @export
-GetQueue <- function(){
+GetQueue <- function(office_acronym="UD_Ochota"){
   # change to import from/depends
   library("jsonlite")  
   
@@ -10,11 +23,9 @@ GetQueue <- function(){
   #ff2f6b11-4eec-4611-a314-d5406bf253fd
   
   url <- "https://api.um.warszawa.pl/api/action/wsstore_get"
+  district_id = office_ids[[office_acronym]]
   
-  id_USC_Andersa <- "5d2e698a-9c31-456b-8452-7ce33e7deb94"
-  id_UD_Białołęka <- "95fee469-79db-4b4b-9ddc-91d49d1f0f51"
-  id_UD_Bielany <- "9c3d5770-57d8-4365-994c-69c5ac4186ee"
-  id_UD_Ochota <- "624d7e2a-bf45-48d6-ba79-8b512e662d1c"
+  if(is.null(district_id)) stop("Unrecognized office acronym!")
   
   wwstore_get_link <- function(id){
     url <- "https://api.um.warszawa.pl/api/action/wsstore_get"
@@ -22,9 +33,11 @@ GetQueue <- function(){
   }
   
   
-  url_id <- wwstore_get_link(id_UD_Ochota)
+  url_id <- wwstore_get_link(district_id)
   
   #url_id <- paste(url, "/?id=", ochota_id, sep = "")
   dane <- jsonlite::fromJSON(url_id)
   dane
 }
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kolejkeR
 API for "Otwarte dane po warszawsku"
+
+
+[![Travis build status](https://travis-ci.org/sowiks2711/RPackage.svg?branch=master)](https://travis-ci.org/sowiks2711/RPackage)
+[![Coverage status](https://codecov.io/gh/sowiks2711/RPackage/branch/master/graph/badge.svg)](https://codecov.io/github/sowiks2711/RPackage?branch=master)
