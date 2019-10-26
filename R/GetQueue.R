398183a56a1878a4981e6c7387c7f260d3c6daa1
sowiks <sowiks@student.mini.pw.edu.pl>
2019-10-26 15:54:26 +0200

    Add spelling check and fix import lib

--- R/GetQueue.R
+++ R/GetQueue.R
@@ -1,6 +1,6 @@
 office_ids <- list(
   USC_Andersa = "5d2e698a-9c31-456b-8452-7ce33e7deb94",
-  UD_Białołęka = "95fee469-79db-4b4b-9ddc-91d49d1f0f51",
+  UD_Bialoleka = "95fee469-79db-4b4b-9ddc-91d49d1f0f51",
   UD_Bielany = "9c3d5770-57d8-4365-994c-69c5ac4186ee",
   UD_Ochota = "624d7e2a-bf45-48d6-ba79-8b512e662d1c"
 )
@@ -13,11 +13,10 @@ GetAvailableOfficeAcronyms <- function(){
 
 #' @title function GetQueue
 #' 
-#' @param office_acronym
+#' @param office_acronym \code{character} acronym of office in Warsaw
 #' @export
 GetQueue <- function(office_acronym="UD_Ochota"){
   # change to import from/depends
-  library("jsonlite")  
   
   # API key, not required for this database
   #ff2f6b11-4eec-4611-a314-d5406bf253fd
