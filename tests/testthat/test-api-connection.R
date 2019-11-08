c8506097a84949556593673094bc3f501478df91
hania <zdula616@gmail.com>
2019-11-08 09:39:37 +0100

    add old tests

--- tests/testthat/test-api-connection.R
+++ tests/testthat/test-api-connection.R
@@ -15,3 +15,16 @@ httptest::with_mock_api({
 
 
 
+#test_that("API call responds with no error", {
+#  expect_error(GetQueue(), NA)
+#})
+#
+#test_that("API call responds with no error", {
+#  expect_error(GetQueue("sadasd"), "Unrecognized office acronym!")
+#})
+#
+#test_that("Every acronym gives valid id", {
+#  for(acronym in GetAvailableOfficeAcronyms()){
+#    expect_error(GetQueue(acronym), NA)
+#  }
+#})  
