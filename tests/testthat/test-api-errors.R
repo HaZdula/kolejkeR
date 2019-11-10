2bedaf20bb518f933838411931b1258839453a41
hania <zdula616@gmail.com>
2019-11-10 17:13:37 +0100

    missing testset

--- tests/testthat/test-api-errors.R
+++ tests/testthat/test-api-errors.R
@@ -1,3 +1,16 @@
+testset <- list(
+  USC_Andersa = "5d2e698a-9c31-456b-8452-7ce33e7deb94",
+  UD_Bialoleka = "95fee469-79db-4b4b-9ddc-91d49d1f0f51",
+  UD_Bielany = "9c3d5770-57d8-4365-994c-69c5ac4186ee",
+  UD_Wlochy = "05e32b8b-273b-4684-8dd0-8cd5c04dbb81",
+  UD_Ursus = "06396204-e8c1-4139-80c0-a099ee3c448f",
+  UD_Targowek = "9b8e3980-2d7d-47b7-8c44-4b5e0ca452fe",
+  UD_Srodmiescie_1 = "78f6290a-0a19-482c-9641-8ac06f49c1c2",
+  UD_Mokotow_2 = "bc83ab5a-0ccc-4e4a-b58d-b821e16df176",
+  USC_Smyczkowa = "b03cf70a-cda7-4fc1-86d3-b9257e78033f"
+)
+
+
 httptest::with_mock_api({
   test_that("get_raw_data throws error if incorrect argument is given", {
     expect_error(get_raw_data("wRonGQnamE"), "Unrecognized office acronym!")
